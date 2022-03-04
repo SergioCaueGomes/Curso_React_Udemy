dcf2473391f91d45d222ffec67107eb2d0da4103
Sergio <caue.sergio@gail.com>
2022-03-04 20:43:57 -0300

    Componente 2

--- src/components/basicos/Primeiro.js
+++ src/components/basicos/Primeiro.js
@@ -1,6 +1,14 @@
 import React from "react"
 
 export default function Primeiro() {
-    return <h2>Primeiro Componente</h2>
+    
+    const msg = 'Seja Bom Vindo'
 
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+            <p>{ msg }</p>
+        </div>
+    ) 
+    
 }
