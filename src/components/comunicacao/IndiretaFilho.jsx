e63dd31de0a28b8c92108e1ab59276f019016b18
Sergio <caue.sergio@gail.com>
2022-03-31 22:09:05 -0300

    Cominicacao Indireta usando mudança de estado 'useState'

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -2,14 +2,13 @@ import React from "react";
 
 export default props => {
 
+    const cb = props.quandoClicar
+    const gerarIdade = () => parseInt(Math.random() * (20)) + 50
+    const gerarNerd = () => Math.random() > 0.5
     return (
         <div>
             <div>Filho</div>
-            <button onClick={
-                function (e) {
-                    props.quandoClicar('João', 53, true)
-                }
-            }>
+            <button onClick={_ => cb('João ', gerarIdade(), gerarNerd())}>
                 Fornecer Informações
             </button>
         </div>
